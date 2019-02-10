71d57c1ae0e379d7f388832fa4f71596d24793f9
Madison Hale <madhale@gmail.com>
2019-02-10 18:22:40 -0500

    installed babel to use ES6 import. Cleaned up processing code Fixed transform issues

---
+++ processor.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import momentTz from 'moment-timezone';
+import moment from 'moment';
+import Papa from 'papaparse';
+
+const parseConfig = {header: true, encoding: "utf8", error: function(errorMessage, file) {console.log(errorMessage);}, transform: transformInputs, complete: completion};
+const outputFilePath = "./data/output.csv";
+
+export function processFile(input) {
+    const fileInput = fs.createReadStream(input);
+    Papa.parse(fileInput, parseConfig);
+}
+
+function completion(results, file) {
+    results.data.forEach((row) => {
+        row.TotalDuration = row.FooDuration + row.BarDuration;
+    });
+
+    let processedOutputData = Papa.unparse(results, parseConfig);
+    console.log(processedOutputData);
+
+    fs.writeFile(outputFilePath, processedOutputData, (err) => {
+        if (err) throw err;
+        console.log('The file has been created here: ' + outputFilePath);
+    });
+}
+
+function transformInputs(value, headerName) {
+    switch(headerName.toLowerCase()) {
+        case 'timestamp':
+            return dateFormatter(value);
+        case 'zip':
+            return value.padStart(5, "0");
+        case 'fullname':
+            return value.toUpperCase();
+        case 'fooduration':
+            return durationSecondsFormatter(value);
+        case 'barduration':
+            return durationSecondsFormatter(value);
+        default:
+            break;
+    }
+    return value;
+}
+
+function dateFormatter(colValue) {
+    const sourceDateTime = moment(colValue, 'MM/DD/YY hh:mm:ss a');
+    const sourceDateTimeZone = momentTz.tz(sourceDateTime, 'America/Los_Angeles');
+    let targetDateTimeZone = sourceDateTimeZone.clone().tz('America/New_York');
+    return targetDateTimeZone.format();
+}
+
+function durationSecondsFormatter(colValue) {
+    return moment.duration(colValue).asSeconds();
+}
--- reader.js
+++ reader.js
@@ -1,9 +1,5 @@
-// import Papa from 'papaparse';
-import {transformInputs} from './transformer';
-
-const Papa = require('papaparse');
-const readline = require('readline');
-const fs = require('fs');
+import readline from 'readline';
+import {processFile} from './processor';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,36 +7,18 @@ const rl = readline.createInterface({
   terminal: false
 });
 
-rl.question('Enter the csv file path ', (answer) => {
-  console.log(`Thank you, ${answer} will now be processed`);
-  // let forcedInput = "./data/sample.csv";
-  processFile(answer);
+rl.question('Enter the file name of the CSV File you would like processed from ./data/ or input 1 or 2 to select from the following CSV files located in ./data/ \n 1: sample.csv \n 2: sample-with-broken-utf8.csv \n', (answer) => {
+  if(answer == '1') {
+    console.log('sample.csv will now be processed \n');
+    processFile("./data/sample.csv");
+  } else if(answer == '2') {
+    console.log('sample-with-broken-utf8.csv will now be processed \n');
+    processFile("./data/sample-with-broken-utf8.csv");
+  } else if(answer) {
+    console.log(`${answer} will now be processed \n`);
+    processFile("./data/" + answer);
+  } else {
+    console.log('No Choice was given. The program is now finished.');
+  }
   rl.close();
 });
-
-// rl.on('line', (line) => {
-//   processFile(line)
-// }).on('close', () => {
-//   process.exit();
-// });
-
-function processFile(input) {
-
-  console.log(input);
-  const fileStream = fs.createReadStream(input); //needs file not path
-  Papa.parse(fileStream, {
-    header: true,
-    encoding: "utf8",
-    error: function(errorMessage, file) {
-      console.log(errorMessage);
-    },
-    transform: transformInputs,
-    complete: function(results) {
-      console.log("Parsing complete:", results)
-      fs.writeFile('./data/output.csv', Papa.unparse(results), (err) => {
-        if (err) throw err;
-        console.log('The file has been created!');
-      });
-    }
-  });  
-}
